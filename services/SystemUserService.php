a7fd8770b1ad17790e4e54689209e2863137956c
sheronfdo <jamithsheron5@gmail.com>
2022-05-02 17:39:44 +0530

    create a systemUser.php
    create systemuser class on this file and create model of system user

---
+++ services/SystemUserService.php
@@ -0,0 +1,89 @@
+<?php
+class SystemUserService
+{
+    private $userId;
+    private $employeeId;
+    private $userTypePositionId;
+    private $username;
+    private $password;
+
+    /**
+     * @return mixed
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param mixed $userId
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmployeeId()
+    {
+        return $this->employeeId;
+    }
+
+    /**
+     * @param mixed $employeeId
+     */
+    public function setEmployeeId($employeeId)
+    {
+        $this->employeeId = $employeeId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserTypePositionId()
+    {
+        return $this->userTypePositionId;
+    }
+
+    /**
+     * @param mixed $userTypePositionId
+     */
+    public function setUserTypePositionId($userTypePositionId)
+    {
+        $this->userTypePositionId = $userTypePositionId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+}
