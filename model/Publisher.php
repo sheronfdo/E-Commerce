b67dcd3238ac2de5d08a5f175691c26f9d231ef1
sheronfdo <jamithsheron5@gmail.com>
2022-05-02 18:03:34 +0530

    create publisher.php
    create a model class to publisher with getter and setters

---
+++ model/Publisher.php
@@ -0,0 +1,72 @@
+<?php
+class Publisher
+{
+    private $publisherId;
+    private $name;
+    private $address;
+    private $contactNo;
+
+    /**
+     * @return mixed
+     */
+    public function getPublisherId()
+    {
+        return $this->publisherId;
+    }
+
+    /**
+     * @param mixed $publisherId
+     */
+    public function setPublisherId($publisherId)
+    {
+        $this->publisherId = $publisherId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param mixed $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContactNo()
+    {
+        return $this->contactNo;
+    }
+
+    /**
+     * @param mixed $contactNo
+     */
+    public function setContactNo($contactNo)
+    {
+        $this->contactNo = $contactNo;
+    }
+}
