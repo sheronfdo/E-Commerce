21dbcfe83ff02b52171180a57a01455d1fa03026
sheronfdo <jamithsheron5@gmail.com>
2022-05-02 18:02:19 +0530

    create category.php
    create a model class to category with getters and setters

---
+++ model/Category.php
@@ -0,0 +1,38 @@
+<?php
+class Category
+{
+    private $categoryId;
+    private $name;
+
+    /**
+     * @return mixed
+     */
+    public function getCategoryId()
+    {
+        return $this->categoryId;
+    }
+
+    /**
+     * @param mixed $categoryId
+     */
+    public function setCategoryId($categoryId)
+    {
+        $this->categoryId = $categoryId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
