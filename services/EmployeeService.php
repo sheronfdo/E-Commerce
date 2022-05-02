e04e3fd1362ee69f8b8e8568bf0b27dd862eb963
sheronfdo <jamithsheron5@gmail.com>
2022-05-02 17:32:29 +0530

    update employeeservice.php
    create a model class to employee

---
+++ services/EmployeeService.php
@@ -0,0 +1,140 @@
+<?php
+class EmployeeService
+{
+    public $employeeId;
+    public $fName;
+    public $lName;
+    public $nic;
+    public $address;
+    public $dateOfBirth;
+    public $gender;
+    public $dateOfRegister;
+
+    /**
+     * @return mixed
+     */
+    public function getEmployeeId()
+    {
+        return $this->employeeId;
+    }
+
+    /**
+     * @param mixed $employeeId
+     */
+    public function setEmployeeId($employeeId)
+    {
+        $this->employeeId = $employeeId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFName()
+    {
+        return $this->fName;
+    }
+
+    /**
+     * @param mixed $fName
+     */
+    public function setFName($fName)
+    {
+        $this->fName = $fName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLName()
+    {
+        return $this->lName;
+    }
+
+    /**
+     * @param mixed $lName
+     */
+    public function setLName($lName)
+    {
+        $this->lName = $lName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNic()
+    {
+        return $this->nic;
+    }
+
+    /**
+     * @param mixed $nic
+     */
+    public function setNic($nic)
+    {
+        $this->nic = $nic;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param mixed $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateOfBirth()
+    {
+        return $this->dateOfBirth;
+    }
+
+    /**
+     * @param mixed $dateOfBirth
+     */
+    public function setDateOfBirth($dateOfBirth)
+    {
+        $this->dateOfBirth = $dateOfBirth;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * @param mixed $gender
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateOfRegister()
+    {
+        return $this->dateOfRegister;
+    }
+
+    /**
+     * @param mixed $dateOfRegister
+     */
+    public function setDateOfRegister($dateOfRegister)
+    {
+        $this->dateOfRegister = $dateOfRegister;
+    }
+}
