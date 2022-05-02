7566902b317af7bdd3aed3d953f7106706363ffa
sheronfdo <jamithsheron5@gmail.com>
2022-05-02 18:03:59 +0530

    create book.php
    create a model class to book with getter and setters

---
+++ model/Book.php
@@ -0,0 +1,89 @@
+<?php
+class Book
+{
+    private $bookId;
+    private $name;
+    private $publisherId;
+    private $categoryId;
+    private $description;
+
+    /**
+     * @return mixed
+     */
+    public function getBookId()
+    {
+        return $this->bookId;
+    }
+
+    /**
+     * @param mixed $bookId
+     */
+    public function setBookId($bookId)
+    {
+        $this->bookId = $bookId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPublisherId()
+    {
+        return $this->publisherId;
+    }
+
+    /**
+     * @param mixed $publisherId
+     */
+    public function setPublisherId($publisherId)
+    {
+        $this->publisherId = $publisherId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCategoryId()
+    {
+        return $this->categoryId;
+    }
+
+    /**
+     * @param mixed $categoryId
+     */
+    public function setCategoryId($categoryId)
+    {
+        $this->categoryId = $categoryId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+}
